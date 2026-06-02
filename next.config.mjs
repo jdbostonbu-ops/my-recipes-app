a58945acdb0dcf4e10e2baf654aab228dad23652
Jacqueline Delgado <jdboston.bu@gmail.com>
2026-06-01 20:46:25 -0400

    updated next.config.mjs for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
-  output: 'export',
-  basePath: '/my-recipes-app',
-  trailingSlash: true,
-  images: { unoptimized: true },
 };
 
 export default nextConfig;
